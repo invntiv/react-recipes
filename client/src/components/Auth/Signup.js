f777d473bc0e940590070c349be4afe32966c778
Josh Mack <jjmack@uwm.edu>
2019-11-03 23:07:37 -0600

    signup complete

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,8 +1,84 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { SIGNUP_USER } from "../../queries";
 
 class Signup extends React.Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: ""
+  };
+  handleChange = event => {
+    const { name, value } = event.target;
+    console.log(name, ":", value);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = (event, signupUser) => {
+    event.preventDefault();
+    signupUser().then(data => {
+      console.log(data);
+    });
+  };
+
   render() {
-    return <div>Signup</div>;
+    const { username, email, password, passwordConfirmation } = this.state;
+
+    return (
+      <div className="App">
+        <h2 className="App">Signup</h2>
+        <Mutation
+          mutation={SIGNUP_USER}
+          variables={{ username, email, password }}
+        >
+          {(signupUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={event => {
+                  this.handleSubmit(event, signupUser);
+                }}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="text"
+                  name="passwordConfirmation"
+                  placeholder="Confirm Password"
+                  value={passwordConfirmation}
+                  onChange={this.handleChange}
+                />
+                <button type="submit" className="button-primary">
+                  Submit
+                </button>
+              </form>
+            );
+          }}
+        </Mutation>
+      </div>
+    );
   }
 }
 
